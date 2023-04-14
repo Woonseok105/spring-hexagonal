f0497058b948976d43da8ee67fe2fdb366723a9f
woonseok105 <yws3786@gmail.com>
2023-04-14 20:09:55 +0900

    create project

---
+++ spring-hexagonal/src/main/kotlin/com/example/springhexagonal/SpringHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springhexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringHexagonalApplication>(*args)
+}
